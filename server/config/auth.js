5e73fb7368d07bf8c09a077217851cf4338254e7
arasan1289 <arasan1289@gmail.com>
2016-05-21 19:03:44 +0530

    fix package.json

--- server/config/auth.js
+++ server/config/auth.js
@@ -3,13 +3,13 @@ module.exports = {
   'googleAuth' : {
     'clientID'      : '66973992743-bcrk5lhrk5aua40ailklrk0rgs89shjh.apps.googleusercontent.com', // your App ID
     'clientSecret'  : 'RARQaAPEW9JstLBZXJbevL5c', // your App Secret
-    'callbackURL'   : 'http://localhost:3000/api/auth/google/callback'
+    'callbackURL'   : 'http://hidden-castle-48503.herokuapp.com/api/auth/google/callback'
   },
 
   'facebook' : {
     'clientID'      : '130971073974370',
     'clientSecret'  : 'e81243bc973bcd9bfabed7f15ffd4ef5',
-    'callbackURL'   : 'http://localhost:3000/api/auth/facebook/callback'
+    'callbackURL'   : 'http://hidden-castle-48503.herokuapp.com/api/auth/facebook/callback'
   }
 
 };
